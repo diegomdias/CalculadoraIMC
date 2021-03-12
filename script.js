28fe6524d93ca744f47626b10fcddb0dba05a6fb
Diego Menezes Dias <79620308+diegomdias@users.noreply.github.com>
2021-03-12 17:07:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function imc(){
   var idade = document.querySelector('#idade');
 var valorIdade = idade.value;
@@ -14,14 +13,84 @@ var calc = valorPeso/(valorAltura*valorAltura);
 var arredondado = parseFloat(calc.toFixed(2));
 
 document.querySelector('#resultado').setAttribute("value", arredondado);
+document.querySelector('#resultado2').setAttribute("value", arredondado);
 
 const textAppear = document.querySelector('#removetext');
-
 textAppear.classList.remove('dnone');
-/*
-const btn2Class = document.querySelector('.btn2');
-btn2Class.textContent = 'recalcular';*/
+
+//IRÁ APARECER O RESULTADO APÓS FUNÇÃO CALCULAR
+const divAppear = document.querySelector('#showme');
+divAppear.classList.replace('dnone', 'tabelaImc');
+
+
+
+if(arredondado < 18.5){
+  const divAppear2 = document.querySelector('#showme2');
+divAppear2.classList.remove('dnone');
+  const alterarTexto = document.querySelector('.alterar');
+alterarTexto.textContent = ', você está abaixo do peso.';
+}
+else if(arredondado >= 18.5 && arredondado < 25 ){
+  const divAppear2 = document.querySelector('#showme2');
+divAppear2.classList.remove('dnone');
+  const alterarTexto = document.querySelector('.alterar');
+  alterarTexto.textContent = ', você está no seu peso normal.';
+
+}
+else if(arredondado >= 25 && arredondado < 30 ){
+  const divAppear2 = document.querySelector('#showme2');
+divAppear2.classList.remove('dnone');
+  const alterarTexto = document.querySelector('.alterar');
+  alterarTexto.textContent = ', você está com um pouco de sobre-peso.';
+
+}
+
+else if(arredondado >= 30 && arredondado < 35 ){
+  const divAppear2 = document.querySelector('#showme2');
+divAppear2.classList.remove('dnone');
+  const alterarTexto = document.querySelector('.alterar');
+  alterarTexto.textContent = ', Obesidade grau 1.';
+
+}
+
+else if(arredondado >= 35 && arredondado < 40 ){
+  const divAppear2 = document.querySelector('#showme2');
+divAppear2.classList.remove('dnone');
+  const alterarTexto = document.querySelector('.alterar');
+  alterarTexto.textContent = ', Obesidade grau 2.';
+
+}
+
+else if(arredondado > 40 ){
+  const divAppear2 = document.querySelector('#showme2');
+divAppear2.classList.remove('dnone');
+  const alterarTexto = document.querySelector('.alterar');
+  alterarTexto.textContent = ', Obesidade grau 3.';
+
+}
+
+else{
+  const divAppear3 = document.querySelector('#showme3');
+  divAppear3.classList.remove('dnone');
+  const alterarTexto2 = document.querySelector('.alterar2');
+  alterarTexto2.textContent = 'Preencha todos os dados corretamente.';
+ 
+  
+
+}
+
 
 
 }
+
 document.addEventListener('', imc);
+
+////////////////////////////////////////////////////////////////////
+
+
+function man(){
+  var clique = document.querySelector('.borderm')
+  clique.classList.toggle('borderm')
+}
+document.addEventListener('', man);
+
